7dafa7c22ac576b43bdf3137f44c54c59ae21e1b
Norberto García Gaspar <alu0100611519@ull.edu.es>
2016-04-21 23:01:59 +0000

    añadido save y find al app.js

--- app.js
+++ app.js
@@ -24,6 +24,45 @@ app.get('/csv', (request, response) => {
   response.send({"rows": calculate(request.query.input)})
 });
 
+
+const Entrada = require('.models/db');
+
+
+app.get('/mongo/', function(req, res) {
+    Entrada.find({}, function(err, docs) {
+        if (err)
+            return err;
+        if (docs.length >= 4) {
+            Entrada.find({ name: docs[3].name }).remove().exec();
+        }
+    });
+    let input = new Entrada({
+        "name": req.query.name,
+        "content": req.query.content
+    });
+
+    input.save(function(err) {
+        if (err) {
+            console.log(`Hubieron errores:\n${err}`);
+            return err;
+        }
+        console.log(`Guardado: ${input}`);
+    });
+});
+
+app.get('/find', function(req, res) {
+    Entrada.find({}, function(err, docs) {
+        if (err)
+            return err;
+        res.send(docs);
+    });
+});
+
+
+// BUSCAR POR NOMBRE 
+
+
+
 app.listen(app.get('port'), () => {
     console.log(`Node app is running at localhost: ${app.get('port')}` );
 });
